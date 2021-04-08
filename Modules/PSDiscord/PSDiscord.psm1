d8d830cb7772b2168f7ba5980e40b13b9130dd51
NotoriousPyro <craigcrawford1988@gmail.com>
2021-04-08 04:21:35 +0100

    first commit

---
+++ Modules/PSDiscord/PSDiscord.psm1
@@ -0,0 +1,185 @@
+function Get-DiscordConfig {
+    [CmdletBinding()]
+    param(
+        [string] $Name = 'Primary'
+    )
+    [string] $Path = [IO.Path]::Combine($Env:USERPROFILE, '.psdiscord')
+    [string] $PathXML = [IO.Path]::Combine($Path, "config.xml")
+
+    if (Test-Path -Path $PathXML) {
+        $Configuration = Import-Clixml -Path $PathXML
+        if ($null -ne $Configuration.$Name) {
+            return $Configuration.$Name
+        }
+    }
+    return $null
+}
+function Initialize-DiscordConfig {
+    [CmdletBinding()]
+    param(
+        [string] $Name = 'Primary',
+        [Uri] $URI
+    )
+    [string] $Path = [IO.Path]::Combine($Env:USERPROFILE, '.psdiscord')
+    [string] $PathXML = [IO.Path]::Combine($Path, "config.xml")
+    # Creates required folder
+
+    $Configuration = @{}
+    $Configuration.$Name = $URI
+
+    $null = New-Item -Path $Path -ItemType Directory -Force
+    Export-Clixml -Path $PathXML -InputObject $Configuration -Force
+}
+function New-DiscordAuthor {
+    [CmdletBinding()]
+    param (
+        [string] $Name,
+        [Uri] $Url,
+        [Uri] $IconUrl,
+        [Uri] $ProxyUrlIcon
+    )
+    $Author = [ordered] @{
+        name           = $Name
+        url            = $Url
+        icon_url       = $IconUrl
+        proxy_icon_url = $ProxyUrlIcon
+    }
+    return $Author
+}
+function New-DiscordFact {
+    [alias("New-DiscordField")]
+    [CmdletBinding()]
+    param (
+        [string] $Name,
+        [string] $Value,
+        [bool] $Inline
+    )
+
+    If ($Name -ne '' -and $Value -ne '') {
+        $Fact = [ordered] @{
+            name   = $Name
+            value  = $Value
+            inline = $inline
+        }
+        return $Fact
+    }
+}
+function New-DiscordImage {
+    [alias('New-DiscordThumbnail')]
+    [CmdletBinding()]
+    param (
+        [Uri] $Url,
+        [int] $Width,
+        [int] $Height
+    )
+    $Thumbnail = [ordered] @{
+        "url"    = $Url
+        "width"  = $Width
+        "height" = $Height
+    }
+    return $Thumbnail
+}
+function New-DiscordSection {
+    [alias("New-DiscordEmbed")]
+    [CmdletBinding()]
+    param (
+        [string] $Title,
+        [string] $Url,
+        [string] $Description,
+        [alias('Fields')][System.Collections.IDictionary[]] $Facts,
+        [RGBColors] $Color,
+        [System.Collections.IDictionary] $Author,
+        [System.Collections.IDictionary] $Thumbnail,
+        [System.Collections.IDictionary] $Image
+    )
+    $Section = [ordered] @{
+        title       = $Title
+        description = $Description
+        fields      = @()
+    }
+    $Field = foreach ($Fact in $Facts) {
+        if ($null -ne $Fact) {
+            $Fact
+        }
+    }
+    $Section.fields = @($Field)
+    if ($null -ne $Color) {
+        $Section.color = ConvertFrom-Color -Color $Color -AsDecimal
+    }
+    if ($null -ne $Author) {
+        $Section.author = $Author
+    }
+    if ($null -ne $Image) {
+        $Section.image = $Image
+    }
+    if ($null -ne $Thumbnail) {
+        $Section.thumbnail = $Thumbnail
+    }
+    if ($null -ne $Url) {
+        $Section.url = $Url
+    }
+    return $Section
+}
+function Send-DiscordMessage {
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [alias('Url', 'Uri')][Uri] $WebHookUrl,
+        [alias('Embeds', 'Embed', 'Section')][System.Collections.IDictionary[]] $Sections,
+        [alias('Content', 'Message')][string] $Text,
+        [alias('Username')] [string] $AvatarName,
+        [Uri] $AvatarUrl,
+        [alias('TTS')][switch] $TextToSpeech,
+        [switch] $CreateConfig,
+        [string] $ConfigName,
+        [switch] $OutputJSON
+    )
+    if ([string]::IsNullOrEmpty($ConfigName)) {
+        $ConfigName = 'Primary'
+    }
+    if (-not $WebHookUrl) {
+        $WebHookUrl = Get-DiscordConfig -Name $ConfigName
+    }
+    if ($null -eq $WebHookUrl) {
+        Write-Warning 'Send-DiscordMessage - WebhookUrl is not set. Either provide it as parameter or initialize it with config.'
+    }
+    if ($CreateConfig) {
+        Initialize-DiscordConfig -Name $ConfigName -URI $WebHookUrl
+    }
+    $FullMessage = [ordered] @{
+        "embeds" = @()
+    }
+    if ($null -ne $Sections) {
+        foreach ($Section in $Sections) {
+            if ($null -ne $Section) {
+                $FullMessage.embeds += $Section
+            }
+        }
+    }
+    if ($null -ne $Text) {
+        if ($TextToSpeech) {
+            # Applies only to Content
+            $FullMessage.tts = $true
+        }
+        $FullMessage.content = $Text
+    }
+    if ($null -ne $AvatarName) {
+        $FullMessage.username = $AvatarName
+    }
+    if ($null -ne $AvatarUrl) {
+        $FullMessage.avatar_url = $AvatarUrl
+    }
+
+    $Body = ConvertTo-Json -Depth 6 -InputObject $FullMessage
+    Write-Verbose -Message "Send-DiscordMessage - Body: `n$Body"
+    if ($PSCmdlet.ShouldProcess("$([System.Environment]::NewLine)$Body", 'Invoke-RestMethod')) {
+        Invoke-RestMethod -Uri $WebHookUrl -Body $Body -Method Post -ContentType "application/json" -Verbose:$false
+    }
+    if ($OutputJSON) {
+        return $Body
+    }
+}
+
+
+Export-ModuleMember `
+    -Function @('New-DiscordAuthor','New-DiscordFact','New-DiscordImage','New-DiscordSection','Send-DiscordMessage') `
+    -Alias @('New-DiscordEmbed','New-DiscordField','New-DiscordThumbnail')
---
+++ eToroTraders.ps1
@@ -0,0 +1,144 @@
+﻿$profilePageUri = "https://www.etoro.com/people/{0}"
+$postPageUri = "https://www.etoro.com/posts/{0}"
+$marketsUri = "https://www.etoro.com/markets/{0}"
+$referrerUri = $profilePageUri + "/portfolio"
+$tradesUri = "https://www.etoro.com/api/streams/v2/streams/user-trades/{0}"
+$workingDir = Split-Path $PSCommandPath -Parent
+$logFile = Join-Path $workingDir (((Split-Path $PSCommandPath -Leaf) -split '\.')[0] + ".log")
+
+$traders = (Get-Content (Join-Path $PSScriptRoot eToroConfig.json) | ConvertFrom-Json).traders
+
+$modules = Join-Path $PSScriptRoot 'Modules'
+if (-not $env:PSModulePath.Contains($modules)) {
+    $env:PSModulePath = $env:PSModulePath + ";" + $modules
+}
+Import-Module PSDiscord
+
+function Log {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Message
+    )
+    $Message | Out-File -FilePath $logFile -Append
+}
+
+function GetDiscordTradeSection {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [PSCustomObject]
+        $Trade
+    )
+
+    if ($Trade.type -eq "CloseTrade") {
+        $tradeType = "Close"
+        if ($Trade.gain -gt 0.0) {
+            $color = [RGBColors]::Green
+        }
+        elseif ($Trade.gain -lt 0.0) {
+            $color = [RGBColors]::Red
+        }
+        $gainFact = New-DiscordFact -Name "Gain" -Value $Trade.gain -Inline $true
+    }
+    else {
+        $tradeType = "Open"
+        if ($trade.direction -eq "Long") {
+            $color = [RGBColors]::Green
+        }
+        elseif ($trade.direction -eq "Short") {
+            $color = [RGBColors]::Red
+        }
+    }
+
+
+
+    $thumbnail = New-DiscordThumbnail -Url $trade.symbol.images.'50X50'
+    $author = New-DiscordAuthor -Name $trade.user.username -Url ($profilePageUri -f $trade.user.username.ToLower()) -IconUrl $trade.user.avatars.small
+    $titleUri = $postPageUri -f $trade.id
+    $title = "{0} {1}" -f $trade.direction, $trade.symbol.marketName
+    
+    $marketUri = $marketsUri -f $trade.symbol.name
+    $marketValue = "[{0}]({1})" -f $trade.symbol.displayName, $marketUri
+
+    $marketFact = New-DiscordFact -Name "Market" -Value $marketValue -Inline $true
+    $rateFact = New-DiscordFact -Name "Rate" -Value $trade.rate -Inline $true
+    $directionFact = New-DiscordFact -Name "Direction" -Value $trade.direction -Inline $true
+    $leverageFact = New-DiscordFact -Name "Leverage" -Value $trade.leverage -Inline $true
+    $categoryFact = New-DiscordFact -Name "Category" -Value $trade.symbol.type -Inline $true
+
+    New-DiscordSection -Title $title -Description "" -Url $titleUri -Facts $rateFact, $directionFact, $leverageFact, $marketFact, $categoryFact -Color:$color -Thumbnail $thumbnail -Author $author
+}
+
+function GetTrades {
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string] $trader
+    )
+    $headers = @{
+        "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.152 Safari/537.36"
+        "Referrer" = $referrerUri -f $trader
+    }
+    $uri = $tradesUri -f $trader
+
+    $request = Invoke-WebRequest -Uri $uri -Headers $headers -UseBasicParsing
+
+    Log -Message ("HTTP status code was: {0}" -f $request.StatusCode)
+
+    return $request
+}
+
+$sections = [Collections.ArrayList]@()
+$hasrun = $false
+
+foreach ($trader in $traders) {
+    if ($hasrun) {
+        Start-Sleep -Seconds 60
+    }
+    $savedTradesFile = Join-Path $workingDir ("traders/{0}.json" -f $trader)
+    $savedTrades = $null
+    if (Test-Path $savedTradesFile) {
+        $savedTrades = Get-Content $savedTradesFile | ConvertFrom-Json
+    }
+
+    $request = GetTrades $trader
+    $hasrun = $true
+    
+    if ($request.StatusCode -ne "200") {
+        Log -Message ("Skipping trader due to failed fetch: {0}" -f $trader)
+        continue
+    }
+
+    $request.Content | Out-File $savedTradesFile
+
+    if ($null -eq $savedTrades) {
+        continue
+    }
+
+    $trades = $request.Content | ConvertFrom-Json
+    $newTrades = $trades | Where-Object { $_.positionid -notin $savedTrades.positionid }
+
+    if ($newTrades.Length -eq 0) {
+        Log -Message ("No new trades for user: {0}" -f $trader)
+        continue
+    }
+
+    foreach ($trade in $newTrades) {
+        $t = GetDiscordTradeSection -Trade $trade
+        $sections.Add($t) | Out-Null
+
+        if ($sections.Count -ge 10) {
+            Send-DiscordMessage -ConfigName etorobot -Sections $sections
+            $sections.Clear()
+        }
+    }
+
+    if ($sections.Count -gt 0) {
+        Send-DiscordMessage -ConfigName etorobot -Sections $sections
+        $sections.Clear()
+    }
+}
